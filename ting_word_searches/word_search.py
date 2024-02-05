40d4d12184a87ebd18bcdae7d96f45a3c34e2fbf
Gustavo <gustavo.t.paula@gmail.com>
2024-02-05 19:34:17 -0300

    baraban

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    lower_word = word.lower()
+    result = []
+
+    for e in instance.queue:
+        occurrences = [
+            {"linha": i + 1}
+            for i, line in enumerate(e["linhas_do_arquivo"])
+            if lower_word in line.lower()
+        ]
+
+        if occurrences:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": e["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return result
 
 
 def search_by_word(word, instance):
